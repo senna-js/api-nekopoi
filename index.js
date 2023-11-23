b23cc97c9eb5d1f00232b59d8e83308f5bea5de4
Wibumoe <131719695+wibumoe@users.noreply.github.com>
2023-11-24 03:21:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ async function getDetailAnime(url) {
   const judul = $("div.bg-white.shadow").find("div.colinfo h2").first().text();
   const jepang = $("div.bg-white.shadow").find("div.colinfo span").first().text();
   const status = $("div.post-body").find("ul.infolist a").first().text();
-  const score = $("div.colinfo").find("div.infolist span:nth-child(3)").text();
-  const duration = $("div.colinfo").find("div.infolist span:nth-child(4)").text();
-  const released = $("div.colinfo").find("div.infolist span:nth-child(5)").text();
-  const producers = $("div.colinfo").find("div.infolist span:nth-child(6)").text();
+  const score = $("div.colinfo").find("ul.infolist span:nth-child(3)").text();
+  const duration = $("div.colinfo").find("ul.infolist span:nth-child(4)").text();
+  const released = $("div.colinfo").find("ul.infolist span:nth-child(5)").text();
+  const producers = $("div.colinfo").find("ul.infolist span:nth-child(6)").text();
   const genres = $("div.colinfo").find("div.genres a").text();
   const sinopsis = $("div.jcontent.jayapanel").find(".sinops").text();
 
